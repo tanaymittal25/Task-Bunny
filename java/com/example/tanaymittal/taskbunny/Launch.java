7479c232b0d4ee6f0d545be7c41bea7a82554dbd
Tanay Agarwal <tanay.mittal25@gmail.com>
2018-12-11 16:46:41 +0530

    Add files via upload

--- java/com/example/tanaymittal/taskbunny/Launch.java
+++ java/com/example/tanaymittal/taskbunny/Launch.java
@@ -1,13 +1,27 @@
 package com.example.tanaymittal.taskbunny;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Launch extends AppCompatActivity {
+
+    private TextView signupText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch);
+
+        signupText = findViewById(R.id.signup_text);
+
+        signupText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), Register.class));
+            }
+        });
     }
 }
---
+++ java/com/example/tanaymittal/taskbunny/Register.java
@@ -0,0 +1,104 @@
+package com.example.tanaymittal.taskbunny;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.firebase.client.Firebase;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Register extends AppCompatActivity {
+
+    private EditText emailReg;
+    private EditText passReg;
+    private EditText rpassReg;
+    private Button btnReg;
+    private TextView loginText;
+
+    private FirebaseAuth mAuth;
+    private ProgressDialog mDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        emailReg = findViewById(R.id.email_register);
+        passReg = findViewById(R.id.password_register);
+        rpassReg = findViewById(R.id.repassword_register);
+        btnReg = findViewById(R.id.button_register);
+        loginText = findViewById(R.id.login_text);
+
+        mAuth = FirebaseAuth.getInstance();
+        mDialog = new ProgressDialog(this);
+
+        loginText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), Launch.class));
+            }
+        });
+
+        btnReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String mEmail = emailReg.getText().toString().trim();
+                String mPass = passReg.getText().toString().trim();
+                String mPassCheck = rpassReg.getText().toString().trim();
+
+                if(TextUtils.isEmpty(mEmail)) {
+                    emailReg.setError("Required Field..");
+                    return;
+                }
+
+                if(TextUtils.isEmpty(mPass)) {
+                    passReg.setError("Required Field..");
+                    return;
+                }
+
+                if(TextUtils.isEmpty(mPassCheck)) {
+                    rpassReg.setError("Required Field..");
+                    return;
+                }
+
+                if(!mPass.equals(mPassCheck)) {
+                    rpassReg.setError("Enter Same Password..");
+                    return;
+                }
+
+                mDialog.setMessage("Processing...");
+                mDialog.show();
+
+                mAuth.createUserWithEmailAndPassword(mEmail, mPass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if(task.isSuccessful()) {
+
+                            Toast.makeText(getApplicationContext(), "Successfully Registered..", Toast.LENGTH_LONG).show();
+                            startActivity(new Intent(getApplicationContext(), Home.class));
+                            mDialog.dismiss();
+
+                        } else {
+
+                            Toast.makeText(getApplicationContext(), "Error..", Toast.LENGTH_LONG).show();
+                            mDialog.dismiss();
+                        }
+                    }
+                });
+            }
+        });
+    }
+}
