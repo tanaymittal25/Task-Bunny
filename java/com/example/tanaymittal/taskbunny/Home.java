4879d86276d6eed69ee50a411e2348fa4f77ed38
Tanay Agarwal <tanay.mittal25@gmail.com>
2018-12-17 22:39:04 +0530

    Add files via upload

--- java/com/example/tanaymittal/taskbunny/Home.java
+++ java/com/example/tanaymittal/taskbunny/Home.java
@@ -1,10 +1,16 @@
 package com.example.tanaymittal.taskbunny;
 
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import java.util.Objects;
 
@@ -27,6 +33,38 @@ public class Home extends AppCompatActivity {
         fabButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                AlertDialog.Builder myDialog = new AlertDialog.Builder(Home.this);
+                LayoutInflater inflater = LayoutInflater.from(Home.this);
+                View myView = inflater.inflate(R.layout.custominputfield, null);
+                myDialog.setView(myView);
+                AlertDialog dialog = myDialog.create();
+
+                final EditText title = myView.findViewById(R.id.text_title);
+                final EditText note = myView.findViewById(R.id.text_note);
+
+                Button save = myView.findViewById(R.id.btn_save);
+
+                save.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                        String mTitle = title.getText().toString().trim();
+                        String mNote = note.getText().toString().trim();
+
+                        if(TextUtils.isEmpty(mTitle)) {
+                            title.setError("Required Field..");
+                            return;
+                        }
+
+                        if(TextUtils.isEmpty(mNote)) {
+                            note.setError("Required Field..");
+                            return;
+                        }
+                    }
+                });
+
+                dialog.show();
 
             }
         });
